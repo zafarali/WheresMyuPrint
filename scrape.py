9ca3aec9d5cb9a193f7c0f202a3d7aded1c65cfa
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2014-03-09 19:00:22 -0400

    fixes json formatting and fixes true false issues

--- scrape.py
+++ scrape.py
@@ -3,7 +3,7 @@ import urllib2
 url = "http://kb.mcgill.ca/kb/article?articleid=2656&source=article&cid=2&islink=true"
 page = urllib2.urlopen(url).read()
 soup = BeautifulSoup(page)
-f = open("public.json", "w")
+f = open("public/public.json", "w")
 f.write("[")
 end = False
 for table in soup.find('table', {"id": "kbTable"}):
@@ -12,14 +12,14 @@ for table in soup.find('table', {"id": "kbTable"}):
 		if end: break
 		tds = tr.find_all('td')
 		try:
-			if tds[3].p == '<p align="center">Y</p>': 
-				guest = "true" 
+			if tds[3].p == None: 
+				guest = "false" 
 			else: 
-				guest= "false"
-			if tds[4].p == '<p align="center">Y</p>': 
-				color="true" 
+				guest= "true"
+			if tds[4].p == None: 
+				color="false" 
 			else: 
-				color="false"
+				color="true"
 			f.write('{"building": "%s", "floor": "%s", "location": "%s", "guest": %s, "color": %s },' % \
 				(tds[0].text, tds[1].text[0], tds[2].text, guest, color))
 		except IndexError:
