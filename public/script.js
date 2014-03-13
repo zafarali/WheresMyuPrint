6cd253a6e168b593e758380b8325bfa83b7011ed
Zafarali Ahmed <zafarali.ahmed@gmail.com>
2014-03-13 10:49:09 -0400

    tries to use MVC seperation

--- public/script.js
+++ public/script.js
@@ -1,12 +1,5 @@
 var UPrintSearchApp = angular.module('App', []);
-var whereAgain2 = function(loc){
-			if(loc == 'Hallway'){
-				loc = " in the hallway";
-			}else{
-				loc = " in room "+loc;
-			}
-		return loc;
-}
+
 UPrintSearchApp.controller('UPCtrl', function($scope, $http){
 	$http.get('/api/printers').success(function(data){
 		for(var row in data){
@@ -34,20 +27,3 @@ UPrintSearchApp.controller('UPCtrl', function($scope, $http){
 	});
 });
 
-/*NEEDS WORK*/
-UPrintSearchApp.filter('patmatch', function(){
-	return function(items, searchText){
-		if(typeof(searchText) == "undefined"){ searchText=""; }
-		var found =[];
-		searchText = String(searchText);
-		var queries = searchText.split(" ");
-		var re = new RegExp('(?=.*\\b'+queries.join('\\b)(?=.*\\b')+'\\b)', 'i');
-		angular.forEach(items, function(item){
-			var tital = item.tit.toLowerCase();
-				if(re.test(tital)) {
-				found.push(item);
-				}
-		});
-		return found;
-	};
-});
